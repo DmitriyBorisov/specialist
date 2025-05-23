23cd785ae8593a9c101a8e26fb6cca8a9b23536a
Dmitriy Borisov <dmitriy.borisov@a-mobile.biz>
2025-05-23 10:59:58 +0300

    Lab4

---
+++ Lab4/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Lab4.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab4.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(Student.All);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Student student = Student.All.Where(s => s.Id == id).SingleOrDefault();
+            if (student == null) return NotFound();
+            return View(student);
+        }
+    }
+}
---
+++ Lab4/Models/Student.cs
@@ -0,0 +1,27 @@
+﻿namespace Lab4.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+
+        public static IList<Student> All = [
+         new Student(1, "Ivan", 18),
+         new Student(2, "John", 19),
+         new Student(3, "Ben", 23),
+         new Student(4, "Antony", 20),
+         new Student(5, "Alex", 17)
+        ];
+
+        public Student() { }
+
+        public Student(int id, string name, int age)
+        {
+            Id = id;
+            Name = name;
+            Age = age;
+        }
+    }
+}
